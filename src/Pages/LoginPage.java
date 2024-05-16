89ddec4efd079603a6cc6d97161746a187d85fb7
Gabor Razvan <marin.gabor04@e-uvt.ro>
2024-05-17 01:56:32 +0300

    modificari la paginile de logare si register

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -4,45 +4,75 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LoginPage extends JFrame implements ActionListener {
     private JTextField usernameField;
     private JPasswordField passwordField;
-    private JButton registerButton;
+    private JLabel registerLink;
 
     public LoginPage() {
-        setTitle("Login Page");
-        setSize(300, 200);
+        setTitle("RestaurantManager");
+        setSize(400, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        JLabel titleLabel1 = new JLabel("Restaurant");
+        JLabel titleLabel2 = new JLabel("Manager");
         JLabel usernameLabel = new JLabel("Username:");
         JLabel passwordLabel = new JLabel("Password:");
         usernameField = new JTextField(20);
         passwordField = new JPasswordField(20);
         JButton loginButton = new JButton("Login");
-        registerButton = new JButton("Register");
+        registerLink = new JLabel("Don't have an account? Register here");
+        registerLink.setForeground(Color.BLUE);
+        registerLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        setLayout(new GridLayout(3, 2));
+        JLabel LoginLabel = new JLabel("Login");
+        LoginLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        titleLabel1.setFont(new Font("Palatino",Font.BOLD, 48));
+        titleLabel2.setFont(new Font("Palatino",Font.BOLD, 48));
+
+        usernameLabel.setBounds(60, 350, 80, 25);
+        usernameField.setBounds(140, 350, 160, 25);
+        passwordLabel.setBounds(60, 380, 80, 25);
+        passwordField.setBounds(140, 380, 160, 25);
+        loginButton.setBounds(140, 410, 80, 25);
+        registerLink.setBounds(80, 450, 220, 25);
+        LoginLabel.setBounds(140,300,200,40);
+        titleLabel1.setBounds(60,150,320,100);
+        titleLabel2.setBounds(90,190,320,100);
+
+        setLayout(null);
         add(usernameLabel);
         add(usernameField);
         add(passwordLabel);
         add(passwordField);
         add(loginButton);
-        add(registerButton);
+        add(registerLink);
+        add(LoginLabel);
+        add(titleLabel1);
+        add(titleLabel2);
 
         loginButton.addActionListener(this);
-        registerButton.addActionListener(this); // Add action listener to the register button
+        loginButton.addActionListener(this);
+        registerLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                openRegisterPage();
+            }
+        });
+    }
+
+    private void openRegisterPage() {
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.setVisible(true);
+        dispose();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == registerButton) {
-            RegisterPage registerPage = new RegisterPage();
-            registerPage.setVisible(true);
-            dispose();
-        } else {
-            // Otherwise, handle the login button click event
             String username = usernameField.getText();
             String password = new String(passwordField.getPassword());
 
@@ -51,6 +81,6 @@ public class LoginPage extends JFrame implements ActionListener {
             } else {
                 JOptionPane.showMessageDialog(this, "Invalid credentials!");
             }
-        }
+
     }
 }
--- src/Pages/RegisterPage.java
+++ src/Pages/RegisterPage.java
@@ -4,18 +4,23 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RegisterPage extends JFrame implements ActionListener {
     private JTextField usernameField;
     private JPasswordField passwordField;
     private JTextField emailField;
+    private JLabel loginLink;
 
     public RegisterPage() {
-        setTitle("Register Page");
-        setSize(300, 250);
+        setTitle("RestaurantManager");
+        setSize(400, 600);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
+        JLabel titleLabel1 = new JLabel("Restaurant");
+        JLabel titleLabel2 = new JLabel("Manager");
         JLabel usernameLabel = new JLabel("Username:");
         JLabel passwordLabel = new JLabel("Password:");
         JLabel emailLabel = new JLabel("Email:");
@@ -23,8 +28,28 @@ public class RegisterPage extends JFrame implements ActionListener {
         passwordField = new JPasswordField(20);
         emailField = new JTextField(20);
         JButton registerButton = new JButton("Register");
+        loginLink = new JLabel("Do you have an account? Login!");
+        loginLink.setForeground(Color.BLUE);
+        loginLink.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
 
-        setLayout(new GridLayout(4, 2));
+        JLabel LoginLabel = new JLabel("Login");
+        LoginLabel.setFont(new Font("Arial", Font.BOLD, 32));
+        titleLabel1.setFont(new Font("Palatino",Font.BOLD, 48));
+        titleLabel2.setFont(new Font("Palatino",Font.BOLD, 48));
+
+        usernameLabel.setBounds(60, 320, 80, 25);
+        usernameField.setBounds(140, 320, 160, 25);
+        emailLabel.setBounds(60, 350, 80, 25);
+        emailField.setBounds(140, 350, 200, 25);
+        passwordLabel.setBounds(60, 380, 80, 25);
+        passwordField.setBounds(140, 380, 160, 25);
+        registerButton.setBounds(140, 410, 120, 25);
+        loginLink.setBounds(100, 450, 220, 25);
+        LoginLabel.setBounds(140,300,200,40);
+        titleLabel1.setBounds(60,150,320,100);
+        titleLabel2.setBounds(90,190,320,100);
+
+        setLayout(null);
         add(usernameLabel);
         add(usernameField);
         add(passwordLabel);
@@ -32,8 +57,23 @@ public class RegisterPage extends JFrame implements ActionListener {
         add(emailLabel);
         add(emailField);
         add(registerButton);
+        add(titleLabel2);
+        add(titleLabel1);
+        add(loginLink);
 
         registerButton.addActionListener(this);
+        loginLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                openLoginPage();
+            }
+        });
+    }
+
+    private void openLoginPage() {
+        LoginPage loginPage = new LoginPage();
+        loginPage.setVisible(true);
+        dispose();
     }
 
     @Override
